0ee1396842fb4cfa31f160da5de731244e7bf338
Rocio <144937216+Rociosantos18@users.noreply.github.com>
2024-01-03 20:51:32 +0000

    last

--- app.js
+++ app.js
@@ -17,25 +17,20 @@ let oneEuroIs = {
     "GBP": 0.87, // british pound
 }
 
-
-// Declaramos una función con el nombre exacto "formEuroToDollar"
 const fromEuroToDollar = function(valueInEuro) {
-    // Convertimos el valor a dólares
     let valueInDollar = valueInEuro * 1.07;
-    // Retornamos el valor en dólares
     return valueInDollar;
 }
-
-function fromDollarToYen(valueInDollar) {
-    let valueInEuro = valueInDollar / oneEuroIs["USD"];
+const fromDollarToYen = function(value){
+    let valueInEuro = value/ oneEuroIs["USD"];
     let valueInYen = valueInEuro * oneEuroIs["JPY"];
-    return valueInYen
+    return valueInYen;
 }
 
-function fromYenToPound(valueInYen) {
+const fromYenToPound = (valueInYen) => {
     let valueInEuro = valueInYen / oneEuroIs["JPY"];
     let valueInPound = valueInEuro * oneEuroIs["GBP"];
-    return valueInPound
+    return valueInPound;
 }
 
-module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound}
+module.exports = { sum, fromEuroToDollar,fromDollarToYen, fromYenToPound}
--- test.js
+++ test.js
@@ -1,15 +1,17 @@
-const { fromEuroToDollar } = require('./app.js');
+const { fromEuroToDollar, fromYenToPound } = require('./app.js');
 
 test("One euro should be 1.07 dollars", function() {
     const expected = 3.5 * 1.07;
     expect(fromEuroToDollar(3.5)).toBe(expected);
 })
-test("One dollar should be 142.90 yen", () => {
+
+test("One dollar should be ",function() {
     const expected = (3.5 / 1.07) * 156.5;
     expect(fromDollarToYen(3.5)).toBe(expected);
 })
 
-test("One yen should be 142.90 pound", () => {
-    const expected = (3.5 / 1.07) * 156.5;
-    expect(fromDollarToYen(3.5)).toBe(expected);
-})
+test("One yen should be 0.019 pound", () => {
+    const expected = (3.5 / 156.5) * 0.87;
+    expect(fromYenToPound(3.5)).toBe(expected);
+})  
+
